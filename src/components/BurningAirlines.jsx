88bb0d2465889e2656448e9210b5b00cd1f02977
Danielle <danisteele1984@gmail.com>
2021-02-10 17:53:17 +1100

    remove imports

--- src/components/BurningAirlines.jsx
+++ src/components/BurningAirlines.jsx
@@ -1,12 +1,6 @@
 import React, {Component} from 'react';
 
 import Nav from './Nav';
-import Flight from './Flight';
-import Flights from './Flights';
-import Plane from './Plane';
-import Reservations from './Reservations';
-import Search from './Search';
-
 class BurningAirlines extends Component {
   render() {
     return (
