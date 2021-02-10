70f14e71776b1949f34a94a087e8ff0b4af8392d
Danielle <danisteele1984@gmail.com>
2021-02-11 10:12:30 +1100

    login page template

--- src/components/BurningAirlines.jsx
+++ src/components/BurningAirlines.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, {Component} from 'react';
 
 import Nav from './Nav';
@@ -5,10 +6,38 @@ class BurningAirlines extends Component {
 
   // Load this from the Rails App with a login form
   state = {
-    loggedIn: true,
+    loggedIn: false,
     firstName: 'Test',
     lastName: 'User',
-    isAdmin: false
+    isAdmin: false,
+    userName: '',
+    password: '',
+    //remove this later
+    isAdminCheck: ''
+  };
+
+  userChange = event => {
+    this.setState({ userName: event.target.value });
+  };
+
+  passwordChange = event => {
+    this.setState({ password: event.target.value });
+  };
+
+  // For testing only
+  isAdminChange = event => {
+    this.setState({ isAdminCheck: event.target.value });
+  };
+
+  handleLogin = event => {
+    //stop page reloading
+    event.preventDefault();
+    // Convert this to AXIOS post to login then set state on response
+    this.setState({
+      firstName: this.state.userName, 
+      loggedIn: true, 
+      isAdmin: this.state.isAdminCheck
+    });
   };
 
   render() {
@@ -22,7 +51,21 @@ class BurningAirlines extends Component {
     else {
       return (
         <div>
-          Login page goes here
+          <form onSubmit={this.handleLogin}>
+            <label>
+              User Name:
+              <input type="text" name="inputUserName" onChange={this.userChange} />
+            </label>
+            <label>
+              Password:
+            <input type="password" name="inputPassword" onChange={this.passwordChange} />
+            </label>
+            <label>
+              Admin Check (Remove later):
+            <input type="checkbox" name="inputIsAdminCheck" onChange={this.isAdminChange} />
+            </label>
+            <button type="submit">Login!</button>
+          </form>
         </div>
       )
     }
