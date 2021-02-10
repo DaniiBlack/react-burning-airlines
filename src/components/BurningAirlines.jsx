91bcca51ce75c116ea3b07bdec8789673d51bce8
Danielle <danisteele1984@gmail.com>
2021-02-10 18:19:27 +1100

    nav and plane

--- src/components/BurningAirlines.jsx
+++ src/components/BurningAirlines.jsx
@@ -2,12 +2,30 @@ import React, {Component} from 'react';
 
 import Nav from './Nav';
 class BurningAirlines extends Component {
+
+  // Load this from the Rails App with a login form
+  state = {
+    loggedIn: true,
+    firstName: 'Test',
+    lastName: 'User',
+    isAdmin: true
+  };
+
   render() {
-    return (
-      <div>
-        <Nav/>
-      </div>
-    )
+    if(this.state.loggedIn) {
+      return (
+        <div>
+          <Nav isAdmin={this.state.isAdmin} firstName={this.state.firstName}/>
+        </div>
+      )
+    }
+    else {
+      return (
+        <div>
+          Login page goes here
+        </div>
+      )
+    }
   }
 }
 
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -14,49 +14,80 @@ import Search from './Search.jsx';
 
 class Nav extends Component {
   render() {
-    return(
-      <Router>
-        <div className="App">
-          <nav className="App-nav">
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/airplanes">Airplanes</Link>
-              </li>
-              <li>
-                <Link to="/flights">Flights</Link>
-              </li>
-              <li>
-                <Link to="/search">Search</Link>
-              </li>
-            </ul>
-          </nav>
+    if(this.props.isAdmin) {
+      return(
+        <Router>
+          <div className="App">
+            <nav className="App-nav">
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/airplanes">Airplanes</Link>
+                </li>
+                <li>
+                  <Link to="/flights">Flights</Link>
+                </li>
+                <li>
+                  Admin
+                </li>
 
-          <Switch>
-            <Route path="/Search">
-              <Search />
-            </Route>
-            <Route path="/Airplanes">
-              <Airplanes />
-            </Route>
-            <Route path="/Reservations">
-              <Reservations />
-            </Route>
-            <Route path="/Flights">
-              <Flights />
-            </Route>
-            <Route path="/Flight">
-              <Flight />
-            </Route>
-            <Route path="/">
-              <div> Home Page </div>
-            </Route>
-          </Switch>
-        </div>
-      </Router>
-    );
+              </ul>
+            </nav>
+
+            <Switch>
+              <Route path="/Search">
+                <Search />
+              </Route>
+              <Route path="/Airplanes">
+                <Airplanes />
+              </Route>
+              <Route path="/Reservations">
+                <Reservations />
+              </Route>
+              <Route path="/Flights">
+                <Flights />
+              </Route>
+              <Route path="/">
+                <div> Home Page for Admin </div>
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+    )}
+    else {
+      return (<Router>
+      <div className="App">
+        <nav className="App-nav">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/search">Search</Link>
+            </li>
+            <li>
+                  {this.props.firstName}
+                </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/Search">
+            <Search />
+          </Route>
+          <Route path="/Reservations">
+            <Reservations />
+          </Route>
+          <Route path="/">
+            <div> Home Page for User </div>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+      )};
+
   }
 }
 export default Nav;
